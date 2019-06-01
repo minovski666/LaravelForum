c874a9103a9f8c1b5137bdf02d3c66c47ff4974f
minovski666 <minovski666@hotmail.com>
2019-06-01 12:59:26 +0200

    commit

---
+++ app/Http/Controllers/RepliesController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reply;
+use App\Thread;
+
+class RepliesController extends Controller
+{
+    /**
+     * Create a new RepliesController instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => 'index']);
+    }
+
+    /**
+     * Fetch all relevant replies.
+     *
+     * @param int    $channelId
+     * @param Thread $thread
+     */
+    public function index($channelId, Thread $thread)
+    {
+        return $thread->replies()->paginate(20);
+    }
+
+    /**
+     * Persist a new reply.
+     *
+     * @param  integer $channelId
+     * @param  Thread  $thread
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store($channelId, Thread $thread)
+    {
+        $this->validate(request(), ['body' => 'required']);
+
+        $reply = $thread->addReply([
+            'body' => request('body'),
+            'user_id' => auth()->id()
+        ]);
+
+        if (request()->expectsJson()) {
+            return $reply->load('owner');
+        }
+
+        return back()->with('flash', 'Your reply has been left.');
+    }
+
+    /**
+     * Update an existing reply.
+     *
+     * @param Reply $reply
+     */
+    public function update(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+
+        $this->validate(request(), ['body' => 'required']);
+
+        $reply->update(request(['body']));
+    }
+
+    /**
+     * Delete the given reply.
+     *
+     * @param  Reply $reply
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+
+        $reply->delete();
+
+        if (request()->expectsJson()) {
+            return response(['status' => 'Reply deleted']);
+        }
+
+        return back();
+    }
+}
